32333e53ea1d788fca8687610b7102eb69ffbbf3
Madison Kendall <lilyzetakovic@gmail.com>
2022-04-24 18:49:04 -0600

    added add roles function

--- index.js
+++ index.js
@@ -168,7 +168,70 @@ function addDepartment() {
 };
 
 // if the user selects to Add a Role
-inquirer 
+function addRole() {
+  inquirer
+  .prompt([
+    type: "input",
+    name: "role",
+    message: "What role would you like to add?",
+    validate: addRole => {
+      if (addRole) {
+        return true;
+      } else {
+        console.log("Please enter a role to add.");
+        return false;
+      }
+    }
+  },
+  type: "input",
+  name: "salary",
+  message: "What is the salary of this role?",
+  validate: roleSalary => {
+    if(isNAN(roleSalary)) {
+      return true;
+    } else {
+      console.log("Please enter the salary for this role.");
+      return false;
+    }
+  }
+}
+  ])
+  .then(answer => {
+    const params = [answer.role, answer.salary];
+
+    // getting the department from the department table
+    const roleSql = `SELECT name, id FROM department`;
+
+    connection.promise().query(roleSql, (err, data) => {
+      if (err) throw err;
+
+      const dept = data.map(({ name, id }) => ({ name: name, value: id }));
+
+      inquirer.prompt([
+        {
+          type: "list",
+          name: "dept",
+          message: "What department is this role in?",
+          choices: dept
+        }
+      ])
+      .then(deptChoice => {
+        const dept = deptChoice.dept;
+        params.push(dept);
+
+        const sql = `INSERT INTO role (title, salary, department_id)
+        VALUES (?, ?, ?)`;
+
+        connection.query(sql, params, (err, result) => {
+          if (err) throw err;
+          console.log("Added " + answer.role + " to roles.");
+
+          viewAllRoles();
+        });
+      });
+    });
+  });
+};
 
 // if the user selects to Add an Employee
 function addEmployee() {
